4e51587ea3898a3d5705b3b05fa9f833c82b93ab
Aditya1711-tech <adityakaneriya1711@gmail.com>
2023-08-04 09:04:59 +0530

    Base case passed

--- test/translateCommands.test.js
+++ test/translateCommands.test.js
@@ -14,3 +14,16 @@ test("should return the correct position and direction when given a single comma
   const actual = translateCommands(commands, initial);
   expect(actual).toStrictEqual(expected);
 });
+test("Base test case", () => {
+  const commands = ["f", "r", "u", "b", "l"];
+  const initial = {
+    position: [0, 0, 0],
+    direction: "N",
+  };
+  const expected = {
+    position: [0, 1, -1],
+    direction: "W",
+  };
+  const actual = translateCommands(commands, initial);
+  expect(actual).toStrictEqual(expected);
+});
